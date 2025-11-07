7b48efc475e24c1a963e6603f616a23a89fcdd2d
Peter Hoggarth <hoggarth.peter@gmail.com>
2025-11-06 21:07:59 -0500

    Rounds image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             alt="Hero Icon"
             width={150}
             height={150}
-            className="mx-auto mb-4"
+            className="mx-auto rounded-full mb-4"
           />
         </section>
       </AnimatedSection>
@@ -34,13 +34,22 @@ export default function Home() {
         <section id="about" className="my-12">
           <h2 className="text-4xl font-bold mb-6 text-center">About Me</h2>
           <p className="text-xl text-center max-w-3xl mx-auto">
-            With 9 years of experience as a Lead Software Engineer, I specialize in the MEAN stack, building robust and scalable applications. I have a passion for mentoring and a proven ability to lead teams in fast-paced, Agile environments. My expertise in Docker, Helm, and Git allows me to streamline deployments and enhance team collaboration. I am a results-oriented leader, dedicated to driving project success through clear communication and a commitment to quality.
+            With 9 years of experience as a Lead Software Engineer, I specialize
+            in the MEAN stack, building robust and scalable applications. I have
+            a passion for mentoring and a proven ability to lead teams in
+            fast-paced, Agile environments. My expertise in Docker, Helm, and
+            Git allows me to streamline deployments and enhance team
+            collaboration. I am a results-oriented leader, dedicated to driving
+            project success through clear communication and a commitment to
+            quality.
           </p>
         </section>
       </AnimatedSection>
       <AnimatedSection>
         <section id="featured-project" className="my-12">
-          <h2 className="text-4xl font-bold mb-6 text-center">Featured Project</h2>
+          <h2 className="text-4xl font-bold mb-6 text-center">
+            Featured Project
+          </h2>
           <div className="max-w-2xl mx-auto">
             <FeaturedProjectCard {...featuredProject} />
           </div>
