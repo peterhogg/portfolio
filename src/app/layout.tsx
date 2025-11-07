861c7fc640871ffd8e2ae8d2f4f7e0007aab1288
Peter Hoggarth <hoggarth.peter@gmail.com>
2025-11-06 21:07:18 -0500

    Suppresses hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${lato.className} flex flex-col min-h-screen`}>
         <ThemeProvider>
           <Navbar />
