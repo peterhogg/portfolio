14a2636fda87c0aca39ff77f5b5c3725cfe58579
Peter Hoggarth <hoggarth.peter@gmail.com>
2025-11-11 20:58:36 -0500

    Changes dark mode toggle color

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useTheme } from 'next-themes';
-import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
-import { useEffect, useState } from 'react';
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
 
 const ThemeToggleButton = () => {
   const { theme, setTheme } = useTheme();
@@ -18,13 +18,13 @@ const ThemeToggleButton = () => {
 
   return (
     <button
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
     >
-      {theme === 'dark' ? (
+      {theme === "dark" ? (
         <SunIcon className="h-6 w-6 text-yellow-500" />
       ) : (
-        <MoonIcon className="h-6 w-6 text-gray-900" />
+        <MoonIcon className="h-6 w-6 text-slate-500" />
       )}
     </button>
   );
